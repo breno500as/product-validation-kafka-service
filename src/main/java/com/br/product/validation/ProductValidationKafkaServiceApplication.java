86b130420d4be3fd897fd5cb46cf86f87b21f90a
breno.mcosta <breno.mcosta@pbh.gov.br>
2024-01-18 15:01:30 -0300

    up

---
+++ src/main/java/com/br/product/validation/ProductValidationKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.product.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductValidationKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductValidationKafkaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/product/validation/ProductValidationKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.product.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductValidationKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
